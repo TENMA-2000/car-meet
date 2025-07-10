751e0d485aab587ea0133d6d66acd8ae376736f6
tenma-2000 <freedomprogrammer.t@gmail.com>
2025-07-10 12:23:32 +0900

    first commit

---
+++ src/main/java/com/example/carmeet/CarmeetApplication.java
@@ -0,0 +1,13 @@
+package com.example.carmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarmeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarmeetApplication.class, args);
+	}
+
+}
