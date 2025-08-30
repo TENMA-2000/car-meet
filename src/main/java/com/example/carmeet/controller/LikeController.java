b92c456c4b7d4dc98f47c04124d4628e674bf1ef
TENMA-2000 <freedomprogrammer.t@gmail.com>
2025-08-30 17:37:25 +0900

    like-funtion commit

--- src/main/java/com/example/carmeet/controller/LikeController.java
+++ src/main/java/com/example/carmeet/controller/LikeController.java
@@ -1,5 +1,68 @@
+
 package com.example.carmeet.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.carmeet.entity.Post;
+import com.example.carmeet.entity.User;
+import com.example.carmeet.service.LikeService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/api/likes")
+@RequiredArgsConstructor
+@Slf4j
 public class LikeController {
 
+	private final LikeService likeService;
+
+	@PostMapping("/{postId}/{userId}")
+	public ResponseEntity<Boolean> addLike(@PathVariable Long postId, @PathVariable Long userId) {
+		log.debug("addLikeリクエスト受信 postId=" + postId + ", userId=" + userId);
+
+		Post post = new Post();
+		post.setPostId(postId);
+		User user = new User();
+		user.setUserId(userId);
+
+		boolean addSuccess = likeService.addLike(post, user) != null;
+		log.debug("addLike処理結果=" + addSuccess);
+
+		return ResponseEntity.ok(addSuccess);
+	}
+
+	@GetMapping("/count/{postId}")
+	public ResponseEntity<Long> countLikes(@PathVariable Long postId) {
+		log.debug("countLikesリクエスト受信 postId=" + postId);
+
+		Post post = new Post();
+		post.setPostId(postId);
+		long count = likeService.countLikes(post);
+
+		log.debug("countLikes結果=" + count);
+		return ResponseEntity.ok(count);
+	}
+
+	@DeleteMapping("/{postId}/{userId}")
+	public ResponseEntity<Boolean> deleteLike(@PathVariable Long postId, @PathVariable Long userId) {
+		log.debug("deleteLikeリクエスト受信 postId=" + postId + ", userId=" + userId);
+
+		Post post = new Post();
+		post.setPostId(postId);
+		User user = new User();
+		user.setUserId(userId);
+
+		boolean deleteSuccess = likeService.deleteLike(post, user);
+		log.debug("deleteLike処理結果=" + deleteSuccess);
+
+		return ResponseEntity.ok(deleteSuccess);
+	}
 }
--- src/test/java/com/example/carmeet/controller/LikeControllerTest.java
+++ src/test/java/com/example/carmeet/controller/LikeControllerTest.java
@@ -1,6 +1,63 @@
 package com.example.carmeet.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
 public class LikeControllerTest {
 
+	@Autowired
+	private MockMvc mockMvc;
 	
+	@Test
+	public void いいねボタンを未ログインで押した場合は401エラーを返す() throws Exception {
+		Long postId = 1L;
+		Long userId = 2L;
+		
+		mockMvc.perform(post("/api/likes/{postId}/{userId}", postId, userId))
+		.andExpect(status().isUnauthorized());
+	}
+	
+	@Test
+	@WithUserDetails("taro.yamada@example.com")
+	public void いいねボタンをログイン済みで押した場合は200OKエラーを返す() throws Exception {
+		Long postId = 1L;
+		Long userId = 2L;
+		
+		mockMvc.perform(post("/api/likes/{postId}/{userId}", postId, userId))
+		.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void いいねを未ログインで削除した場合は401エラーを返す() throws Exception {
+		Long postId = 1L;
+		Long userId = 2L;
+		
+		mockMvc.perform(delete("/api/likes/{postId}/{userId}", postId, userId))
+		.andExpect(status().isUnauthorized());
+	}
+	
+	@Test
+	@WithUserDetails("taro.yamada@example.com")
+	public void いいねをログイン済みで削除した場合は200OKエラーを返す() throws Exception {
+		Long postId = 1L;
+		Long userId = 2L;
+		
+		mockMvc.perform(delete("/api/likes/{postId}/{userId}", postId, userId))
+		.andExpect(status().isOk());
+	}
 }
