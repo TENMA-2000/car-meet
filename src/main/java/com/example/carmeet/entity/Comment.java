91fdadaebf4331354b054d5b04fae3f48c09d2c8
TENMA-2000 <freedomprogrammer.t@gmail.com>
2025-08-30 16:21:23 +0900

    like-function

---
+++ src/main/java/com/example/carmeet/entity/Comment.java
@@ -0,0 +1,13 @@
+package com.example.carmeet.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "comment")
+@Data
+public class Comment {
+
+	
+}
